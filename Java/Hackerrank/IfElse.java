e5f00912186d2f545fe73168bd83ffe9b7035545
Yisu Kim <ykim6600@gmail.com>
2018-08-19 20:00:51 +0900

    Create IfElse.java

---
+++ Java/Hackerrank/IfElse.java
@@ -0,0 +1,28 @@
+
+// If-Else
+// https://www.hackerrank.com/challenges/java-if-else/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class IfElse {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if (N % 2 != 0 | (6 <= N & N <= 20)) {
+            System.out.println("Weird");
+        } else if ((2 <= N & N <= 5) | N > 20) {
+            System.out.println("Not Weird");
+        }
+        scanner.close();
+    }
+}
