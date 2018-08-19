2fe6290ca470ed83c82b3c1dcad48873b5d5339b
Yisu Kim <ykim6600@gmail.com>
2018-08-19 19:56:34 +0900

    Create HelloWorld.java
    hackerrank java programming course start

---
+++ Java/Hackerrank/HelloWorld.java
@@ -0,0 +1,10 @@
+// Welcome to Java!
+// https://www.hackerrank.com/challenges/welcome-to-java/problem
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
