dabf99bfe2b55d65213747f786ab07b2af34ca1f
yisu-kim <ykim6600@gmail.com>
2018-09-22 22:22:54 +0900

    Create ReverseLinkedList.java

---
+++ InterviewQuestions/LinkedLists/ReverseLinkedList.java
@@ -0,0 +1,27 @@
+// Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+// O(n) complexity
+// code by https://leetcode.com/problems/reverse-linked-list/solution/
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
