9f5ac5fdf421b7af0eedfa5ac852e85ac749f750
yisu-kim <ykim6600@gmail.com>
2018-09-26 17:54:28 +0900

    Create MaximumDepthOfBinaryTree

---
+++ InterviewQuestions/Trees/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,22 @@
+// Maximum Depth of Binary Tree
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// Recursive Solution
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
