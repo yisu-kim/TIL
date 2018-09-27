b012596a5545e2b9182f424a43eb40f83a18059d
yisu-kim <ykim6600@gmail.com>
2018-09-27 15:14:25 +0900

    Add in-order traversal solution

--- InterviewQuestions/Trees/ValidateBinarySearchTree.java
+++ InterviewQuestions/Trees/ValidateBinarySearchTree.java
@@ -11,7 +11,8 @@
  * }
  */
 
-// O(|V|) = O(|E|) complexity
+// O(|V|) time complexity
+// O(|V|) space complexity
 class Solution {
     public boolean isValidBST(TreeNode root) {
         long minValue = (long) Integer.MIN_VALUE - 1;
@@ -27,5 +28,28 @@ class Solution {
             return false;
         }
         return isValidBST(minValue, root.left, root.val) && isValidBST(root.val, root.right, maxValue);
+    }
+
+    // In-order traversal solution with stack
+    // code by https://leetcode.com/problems/validate-binary-search-tree/discuss/32112/Learn-one-iterative-inorder-traversal-apply-it-to-multiple-tree-questions-(Java-Solution)
+    public boolean isValidBSTInOrderSearch(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (pre != null && root.val <= pre.val) {
+                return false;
+            }
+            pre = root;
+            root = root.right;
+        }
+        return true;
     }
 }
