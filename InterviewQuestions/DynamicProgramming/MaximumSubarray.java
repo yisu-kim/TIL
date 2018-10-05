f7a3bb2e0b2a54fa03fc3801a8099ab3d9f6648b
yisu-kim <ykim6600@gmail.com>
2018-10-06 03:39:40 +0900

    Create MaximumSubarray.java

---
+++ InterviewQuestions/DynamicProgramming/MaximumSubarray.java
@@ -0,0 +1,27 @@
+/**
+ * Maximum Subarray
+ * https://leetcode.com/problems/maximum-subarray/
+ */
+
+/**
+ * O(n) time complexity
+ * O(1) space complexity
+ */
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxEndingHere = 0;
+        int maxSoFar = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (maxEndingHere > 0) {
+                maxEndingHere += nums[i];
+            } else {
+                maxEndingHere = nums[i];
+            }
+            if (maxSoFar < maxEndingHere) {
+                maxSoFar = maxEndingHere;
+            }
+        }
+        return maxSoFar;
+    }
+}
