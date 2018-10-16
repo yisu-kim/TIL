5f50e58faf58717e83e07a97a66c4fcb7aa23457
yisu-kim <ykim6600@gmail.com>
2018-10-17 06:05:34 +0900

    Add show file list

--- Web/Web2-Nodejs/main.js
+++ Web/Web2-Nodejs/main.js
@@ -10,8 +10,25 @@ var app = http.createServer(function (request, response) {
     title = 'Welcome';
   }
   if (pathname === '/') {
-    fs.readFile(`data/${title}`, 'utf8', function (err, description) {
-      var template = `
+    fs.readdir('./data', function (err, filelist) {
+      var list = '<ol>';
+      var i = 0;
+      while (i < filelist.length) {
+        list = list + `<li><a href='/?id=${filelist[i]}'>${filelist[i]}</a></li>`;
+        i = i + 1;
+      }
+      list = list + '</ol>';
+      fs.readFile(`./data/${title}`, 'utf8', function (err, description) {
+        if (description === undefined) {
+          description = `<p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are
+          identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the
+          Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser
+          computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN
+          in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in
+          August 1991.
+          </p>`
+        }
+        var template = `
         <!doctype html>
         <html>
         <head>
@@ -20,18 +37,15 @@ var app = http.createServer(function (request, response) {
         </head>
         <body>
             <h1><a href="/">WEB</a></h1>
-            <ol>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
+            ${list}
             <h2>${title}</h2>
             ${description}
         </body>
         </html>
         `;
-      response.writeHead(200);
-      response.end(template);
+        response.writeHead(200);
+        response.end(template);
+      })
     })
   } else {
     response.writeHead(404);
