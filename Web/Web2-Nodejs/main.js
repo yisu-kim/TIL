89f9e63bbfaec4745e69c4c69c2413e59d76c689
yisu-kim <ykim6600@gmail.com>
2018-10-18 02:14:32 +0900

    Add security for input data

--- Web/Web2-Nodejs/main.js
+++ Web/Web2-Nodejs/main.js
@@ -3,19 +3,22 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
+var path = require('path');
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
   var title = queryData.id;
+  if (title === undefined) {
+    title = 'Welcome';
+  } else {
+    var filteredTitle = path.parse(title).base;
+  }
   if (pathname === '/') {
-    if (title === undefined) {
-      title = 'Welcome';
-    }
     fs.readdir('./data', function (err, filelist) {
       var list = template.list(filelist);
-      fs.readFile(`./data/${title}`, 'utf8', function (err, description) {
+      fs.readFile(`./data/${filteredTitle}`, 'utf8', function (err, description) {
         if (description === undefined) {
           description = `The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.`
         }
@@ -25,7 +28,7 @@ var app = http.createServer(function (request, response) {
         var control = '<a href="/create">create</a>';
         if (title != 'Welcome') {
           control = control
-            + ` <a href="/update?id=${title}">update</a>`
+            + ` <a href="/update?id=${filteredTitle}">update</a>`
             + ` <form action="/delete_process" method="post">
                       <p><input type="hidden" name="id" value=${title}></p>
                       <p><input type="submit" value="delete"></p>
@@ -63,15 +66,16 @@ var app = http.createServer(function (request, response) {
     request.on('end', function () {
       var post = qs.parse(body);
       var title = post.title;
+      var filteredTitle = path.parse(title).base;
       var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-        response.writeHead(302, { Location: `/?id=${title}` });
+      fs.writeFile(`data/${filteredTitle}`, description, 'utf8', function (err) {
+        response.writeHead(302, { Location: `/?id=${filteredTitle}` });
         response.end();
       });
     });
   } else if (pathname === '/update') {
     fs.readdir('./data', function (err, filelist) {
-      fs.readFile(`./data/${title}`, 'utf8', function (err, description) {
+      fs.readFile(`./data/${filteredTitle}`, 'utf8', function (err, description) {
         var list = template.list(filelist);
         var body = `
         <form action="/update_process" method="post">
@@ -98,11 +102,13 @@ var app = http.createServer(function (request, response) {
     request.on('end', function () {
       var post = qs.parse(body);
       var id = post.id;
+      var filteredId = path.parse(id).base;
       var title = post.title;
+      var filteredTitle = path.parse(title).base;
       var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function (err) {
-        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-          response.writeHead(302, { Location: `/?id=${title}` });
+      fs.rename(`data/${filteredId}`, `data/${filteredTitle}`, function (err) {
+        fs.writeFile(`data/${filteredTitle}`, description, 'utf8', function (err) {
+          response.writeHead(302, { Location: `/?id=${filteredTitle}` });
           response.end();
         });
       });
@@ -115,7 +121,8 @@ var app = http.createServer(function (request, response) {
     request.on('end', function () {
       var post = qs.parse(body);
       var id = post.id;
-      fs.unlink(`data/${id}`, function (err) {
+      var filteredId = path.parse(id).base;
+      fs.unlink(`data/${filteredId}`, function (err) {
         response.writeHead(302, { Location: '/' });
         response.end();
       });
