eae2b81c51d7c449eb487fc15184b62c328087a7
yisu-kim <ykim6600@gmail.com>
2018-10-17 17:51:07 +0900

    Add extract post data

--- Web/Web2-Nodejs/main.js
+++ Web/Web2-Nodejs/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateList(filelist) {
   var list = '<ol>';
@@ -58,15 +59,14 @@ var app = http.createServer(function (request, response) {
         var template = templateHTML(title, list, body);
         response.writeHead(200);
         response.end(template);
-      })
-    })
+      });
+    });
   } else if (pathname === '/create') {
     title = 'create'
     fs.readdir('./data', function (err, filelist) {
       var list = templateList(filelist);
-      fs.readFile(`./data/${title}`, 'utf8', function (err, description) {
-        var body = `
-        <form action="http://localhost:3000/process_create" method="post">
+      var body = `
+        <form action="http://localhost:3000/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"></p>
           <p>
             <textarea name="description" placeholder="description"></textarea>
@@ -76,14 +76,25 @@ var app = http.createServer(function (request, response) {
           </p>
         </form>
         `;
-        var template = templateHTML(title, list, body);
-        response.writeHead(200);
-        response.end(template);
-      })
-    })
+      var template = templateHTML(title, list, body);
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if (pathname === '/create_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body = body + data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+    });
+    response.writeHead(200);
+    response.end('Success');
   } else {
     response.writeHead(404);
-    response.end("Not found");
+    response.end('Not found');
   }
 });
 app.listen(3000);
