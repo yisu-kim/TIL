5e4c4e58d3f0d8999858c8a27351f7e4b4aa1ab5
yisu-kim <ykim6600@gmail.com>
2018-10-17 18:01:43 +0900

    Add create file and redirection

--- Web/Web2-Nodejs/main.js
+++ Web/Web2-Nodejs/main.js
@@ -89,9 +89,11 @@ var app = http.createServer(function (request, response) {
       var post = qs.parse(body);
       var title = post.title;
       var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+        response.writeHead(302, {Location: `/?id=${title}`});
+        response.end();
+      });
     });
-    response.writeHead(200);
-    response.end('Success');
   } else {
     response.writeHead(404);
     response.end('Not found');
