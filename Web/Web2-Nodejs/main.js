2e2dba6a5fc53a69bc06be6996265b006562d49e
yisu-kim <ykim6600@gmail.com>
2018-10-17 20:09:21 +0900

    Add delete function

--- Web/Web2-Nodejs/main.js
+++ Web/Web2-Nodejs/main.js
@@ -53,8 +53,8 @@ var app = http.createServer(function (request, response) {
         var control = '<a href="/create">create</a>';
         if (title != 'Welcome') {
           control = control
-                  + ` <a href="/update?id=${title}">update</a>`
-                  + ` <form action="/delete_process" method="post">
+            + ` <a href="/update?id=${title}">update</a>`
+            + ` <form action="/delete_process" method="post">
                       <p><input type="hidden" name="id" value=${title}></p>
                       <p><input type="submit" value="delete"></p>
                       </form>`;
@@ -128,11 +128,24 @@ var app = http.createServer(function (request, response) {
       var id = post.id;
       var title = post.title;
       var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function(err) {
+      fs.rename(`data/${id}`, `data/${title}`, function (err) {
         fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
           response.writeHead(302, { Location: `/?id=${title}` });
           response.end();
         });
+      });
+    });
+  } else if (pathname === '/delete_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body = body + data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function(err) {
+        response.writeHead(302, { Location: `/` });
+        response.end();
       });
     });
   } else {
