99a979bc147bfbe36b2b32e7dfbf570777f75182
yisu-kim <ykim6600@gmail.com>
2018-10-17 19:08:08 +0900

    Add a update form

--- Web/Web2-Nodejs/main.js
+++ Web/Web2-Nodejs/main.js
@@ -14,7 +14,7 @@ function templateList(filelist) {
   return list;
 }
 
-function templateHTML(title, list, body, control='') {
+function templateHTML(title, list, body, control = '') {
   return `
   <!doctype html>
   <html>
@@ -26,7 +26,7 @@ function templateHTML(title, list, body, control='') {
       <h1><a href="/">WEB</a></h1>
       ${list}
       ${control}
-      ${body}
+      <p>${body}</p>
   </body>
   </html>
   `;
@@ -45,13 +45,7 @@ var app = http.createServer(function (request, response) {
       var list = templateList(filelist);
       fs.readFile(`./data/${title}`, 'utf8', function (err, description) {
         if (description === undefined) {
-          description = `<p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are
-          identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the
-          Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser
-          computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN
-          in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in
-          August 1991.
-          </p>`
+          description = `The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.`
         }
         var body = `
           <h2>${title}</h2>
@@ -70,7 +64,7 @@ var app = http.createServer(function (request, response) {
     fs.readdir('./data', function (err, filelist) {
       var list = templateList(filelist);
       var body = `
-        <form action="http://localhost:3000/create_process" method="post">
+        <form action="/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"></p>
           <p>
             <textarea name="description" placeholder="description"></textarea>
@@ -93,9 +87,30 @@ var app = http.createServer(function (request, response) {
       var post = qs.parse(body);
       var title = post.title;
       var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-        response.writeHead(302, {Location: `/?id=${title}`});
+      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+        response.writeHead(302, { Location: `/?id=${title}` });
         response.end();
+      });
+    });
+  } else if (pathname === '/update') {
+    fs.readdir('./data', function (err, filelist) {
+      fs.readFile(`./data/${title}`, 'utf8', function (err, description) {
+        var list = templateList(filelist);
+        var body = `
+        <form action="/update_process" method="post">
+          <p><input type="hidden" name="id" value=${title}></p>
+          <p><input type="text" name="title" placeholder="title" value=${title}></p>
+          <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `;
+        var template = templateHTML(title, list, body);
+        response.writeHead(200);
+        response.end(template);
       });
     });
   } else {
