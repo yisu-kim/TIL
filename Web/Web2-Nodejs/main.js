7395cc0efc753ada5e5507848725421c20e1f2cd
yisu-kim <ykim6600@gmail.com>
2018-10-19 03:44:49 +0900

    Change a mysql connection settings file

--- Web/Web2-Nodejs/main.js
+++ Web/Web2-Nodejs/main.js
@@ -1,14 +1,8 @@
 var http = require('http');
-var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
-var mysql = require('mysql');
-var connection = require('./lib/connection');
-var db = mysql.createConnection(connection);
-db.connect();
+var db = require('./lib/db');
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
