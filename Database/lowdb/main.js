b5328a7be3a277d15911ffea547bd8b5d407788a
y_kim <ykim6600@gmail.com>
2018-10-23 08:06:25 +0900

    Initialize

--- Database/lowdb/main.js
+++ Database/lowdb/main.js
@@ -7,6 +7,8 @@ const hostname = 'localhost';
 const port = 3000;
 const fs = require('fs');
 
+db.defaults({ topic: [], author: [] }).write();
+
 const server = http.createServer((req, res) => {
     fs.readFile('./web.html', (err, data) => {
         res.statusCode = 200;
