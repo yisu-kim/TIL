bc356e3bc0040bc84ebb6f10c7dd3ef740e79496
y_kim <ykim6600@gmail.com>
2018-10-23 08:24:23 +0900

    Read data

--- Database/lowdb/main.js
+++ Database/lowdb/main.js
@@ -8,16 +8,20 @@ const port = 3000;
 const fs = require('fs');
 
 db.defaults({ topic: [], author: [] }).write();
-db.get('author')
-  .push({ id: 1, name: 'egoing', profile: 'developer' })
-  .write();
-db.get('topic')
-  .push({ id: 1, title: 'lowdb', description: 'lowdb is ...' })
-  .write();
-db.get('topic')
-  .push({ id: 1, title: 'mysql', description: 'mysql is ...' })
-  .write();
-
+// db.get('author')
+//   .push({ id: 1, name: 'egoing', profile: 'developer' })
+//   .write();
+// db.get('topic')
+//   .push({ id: 1, title: 'lowdb', description: 'lowdb is ...', author: 1 })
+//   .write();
+// db.get('topic')
+//   .push({ id: 1, title: 'mysql', description: 'mysql is ...', author: 1 })
+//   .write();
+console.log(
+  db.get('topic')
+    .find({ title: 'lowdb', author: 1 })
+    .value()
+);
 const server = http.createServer((req, res) => {
   fs.readFile('./web.html', (err, data) => {
     res.statusCode = 200;
