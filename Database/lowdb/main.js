72552c80ba482f22cbab5b360684b6107a455408
y_kim <ykim6600@gmail.com>
2018-10-23 08:14:20 +0900

    Create data

--- Database/lowdb/main.js
+++ Database/lowdb/main.js
@@ -8,12 +8,21 @@ const port = 3000;
 const fs = require('fs');
 
 db.defaults({ topic: [], author: [] }).write();
+db.get('author')
+  .push({ id: 1, name: 'egoing', profile: 'developer' })
+  .write();
+db.get('topic')
+  .push({ id: 1, title: 'lowdb', description: 'lowdb is ...' })
+  .write();
+db.get('topic')
+  .push({ id: 1, title: 'mysql', description: 'mysql is ...' })
+  .write();
 
 const server = http.createServer((req, res) => {
-    fs.readFile('./web.html', (err, data) => {
-        res.statusCode = 200;
-        res.end(data);
-    })
+  fs.readFile('./web.html', (err, data) => {
+    res.statusCode = 200;
+    res.end(data);
+  })
 })
 
 server.listen(port, hostname);
